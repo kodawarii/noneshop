23a6f25ce347bb02c4995388cb43b8826e4a94fc
Paul <bama_boy2@hotmail.com>
2020-01-30 22:29:46 +1300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Copy instagram and other designer websites - that really simplistic, elegant style e.g. hermes, balenciaga etc.</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
